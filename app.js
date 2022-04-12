fbad3ee4da6a6c14cb277004a6f8d486f93ff09d
Egor Lukin <86829973+Marvin811@users.noreply.github.com>
2022-04-12 22:27:52 +0300

    feat: mestodb

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
 
-const app = express()
+const { PORT = 3000 } = process.env;
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  autoIndex: true,
+});
 
 app.get('/', function (req, res) {
-  res.send('Hello World !!!')
-})
+  res.send('Hello World !!!');
+});
 
-app.listen(3000)
+app.post('/', function (req, res) {
+  res.send(req.body);
+});
+
+app.listen(PORT);
