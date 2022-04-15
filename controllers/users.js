a87d53b23c140582f44c628c234d5bab8458d225
Egor Lukin <86829973+Marvin811@users.noreply.github.com>
2022-04-15 16:53:42 +0300

    feat: error on users

--- controllers/users.js
+++ controllers/users.js
@@ -1,36 +1,88 @@
+const BadRequestError = require('../errors/badRequestError');
+const NotFoundError = require('../errors/notFoundError');
 const User = require('../models/user');
 
 module.exports.getUsers = (req, res, next) => {
   User.find({})
-    .then((users) => {
-      res.send(users.map((user) => {
-        const {
-          name,
-          about,
-          avatar,
-          _id,
-        } = user;
-        return {
-          _id,
-          name,
-          about,
-          avatar,
-        };
-      }));
-    })
-    .catch(next);
+    .then((users) => res.status(200).send(users))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Переданы некоректные данные.'));
+      } else {
+        next(err);
+      }
+    });
 };
 
-module.exports.getIdUsers = (req, res) => {
+module.exports.getIdUsers = (req, res, next) => {
   User.findById(req.params.id)
-    .then((users) => res.send({ users }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((users) => {
+      if (users) {
+        res.status(200)
+          .send(users);
+      } else {
+        next(new NotFoundError('Пользователь по указанному _id не найден'));
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Переданы некорректные данные.'));
+      } else {
+        next(err);
+      }
+    });
 };
 
-module.exports.createUsers = (req, res) => {
+module.exports.createUsers = (req, res, next) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((users) => res.send({ users }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((users) => res.status(200).send({ users }))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Переданы некоректные данные.'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports.updateUserInfo = (req, res, next) => {
+  const { name, about } = req.body;
+
+  User.findByIdAndUpdate(req.user._id, { name, about })
+    .then((users) => {
+      if (users) {
+        res.status(200).send(users);
+      } else {
+        next(new BadRequestError('Переданы некоректные данные.'));
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new NotFoundError('Пользователь с указанным _id не найден.'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports.updateAvatar = (req, res, next) => {
+  const { avatar } = req.body;
+
+  User.findByIdAndUpdate(req.user._id, { avatar })
+    .then((users) => {
+      if (users) {
+        res.status(200).send(users);
+      } else {
+        next(new BadRequestError('Переданы некоректные данные.'));
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new NotFoundError('Пользователь с указанным _id не найден.'));
+      } else {
+        next(err);
+      }
+    });
 };
