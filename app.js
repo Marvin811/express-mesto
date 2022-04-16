e3d126a8c7981403903473ba1ad68fea91d1d340
Egor Lukin <86829973+Marvin811@users.noreply.github.com>
2022-04-16 12:54:24 +0300

    fix: app.use

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// eslint-disable-next-line import/no-extraneous-dependencies
 const bodyParser = require('body-parser');
+
+const cardRoutes = require('./routes/cards');
+const userRoutes = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use((req, res, next) => {
   req.user = {
     _id: '6258126030a5f4c1433895da', // вставьте сюда _id созданного в предыдущем пункте пользователя
@@ -18,12 +21,12 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/users', require('./routes/users'));
-app.use('/cards', require('./routes/cards'));
-
-app.use((req, res) => {
-  res.status(404).send({ message: 'Страница по указанному маршруту не найдена' });
+app.use('/users', userRoutes);
+app.use('/cards', cardRoutes);
+app.use('/', (req, res) => {
+  res.status(404).send({ message: 'Ошибка: данный ресурс на найден.' });
 });
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('Ссылка на сервер');
