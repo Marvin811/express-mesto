5c18fd0d918e3640341299d9fe48e8728a969368
Egor Lukin <86829973+Marvin811@users.noreply.github.com>
2022-04-25 16:38:26 +0300

    Revert "Revert "feat: login""
    This reverts commit 83be0de928bfbdd3baf6d5d2355247e1fa6239c7.

--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,79 @@
+/* eslint-disable no-unused-vars */
 const { NODE_ENV, JWT_SECRET } = process.env;
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
+const ValidationError = require('../errors/validationError');
+
 const ERROR_NOT_FOUND = 404;
 const BAD_REQUEST = 400;
 const INTERNAL_SERVER_ERR = 500;
 
+module.exports.createUser = (req, res, next) => {
+  const {
+    name,
+    about,
+    avatar,
+    email,
+    password,
+  } = req.body;
+  if (!email || !password) {
+    next(new ValidationError('Не передан email или пароль'));
+  }
+  // хешируем пароль
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
+    }))
+    .then((user) => res.status(200).send({
+      data: user,
+    }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new ValidationError(`${Object.values(err.errors).map((error) => error.message).join(', ')}`));
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        { expiresIn: '7d' },
+      );
+      res
+        .cookie('token', token, {
+          maxAge: 3600000 * 24 * 7,
+          // httpOnly: true,
+        })
+        .send({ token });
+    })
+    .catch((err) => {
+      res.status(401).send({ message: 'ошибка 401' });
+    });
+};
+
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.status(200).send({ data: users }))
+    .then((users) => res.send(users.map((user) => {
+      const {
+        name, about, avatar, _id,
+      } = user;
+      return {
+        _id, name, about, avatar,
+      };
+    })))
     .catch((err) => {
       if (err.name === 'CastError') {
         res.status(BAD_REQUEST).send({ message: 'Нет таких пользователей' });
@@ -102,37 +166,5 @@ module.exports.updateAvatar = (req, res) => {
         res.status(BAD_REQUEST).send({ message: 'передан некорректный id' });
       }
       res.status(INTERNAL_SERVER_ERR).send({ message: 'Что-то пошло не так' });
-    });
-};
-
-module.exports.createUser = (req, res) => {
-  bcrypt.hash(req.body.password, 10)
-    .then((hash) => User.create({
-      email: req.body.email,
-      password: hash,
-    }))
-    .then((user) => res.send(user))
-    .catch((err) => res.status(BAD_REQUEST).send(err));
-};
-
-module.exports.login = (req, res) => {
-  const { email, password } = req.body;
-
-  User.findUserByCredentials(email, password)
-    .then((user) => {
-      const token = jwt.sign(
-        { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
-        { expiresIn: '7d' },
-      );
-      res
-        .cookie('token', token, {
-          maxAge: 3600000 * 24 * 7,
-          // httpOnly: true,
-        })
-        .send({ token });
-    })
-    .catch((err) => {
-      res.status(401).send({ message: err.message });
     });
 };
--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const validator = require('validator');
+const AuthError = require('../errors/authError');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -36,19 +37,18 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.statics.findUserByCredentials = function getUserIfAuth(email, password) {
+userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
+        return Promise.reject(new AuthError('Неправильные почта или пароль'));
       }
 
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new Error('Неправильные почта или пароль'));
+            return Promise.reject(new AuthError('Неправильные почта или пароль'));
           }
-
           return user;
         });
     });
